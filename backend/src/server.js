8a63d15d6dd965f8ab3378caf6508a8b763571df
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-03-23 09:43:38 -0300

    Primeira Rota - Raiz "/" - na Aplicação.
    Criada a primeira Rota "/" utilizando Método GET (solicitação de dados) com os parâmetros "request" (todas as informações referentes à Requisição do usuário) e "response" (retorno da Resposta à Requisição do usuário);
    Criado o Retorno com o parâmetro "response" e com o Método ".send" para exibir um texto simples (Hello World) em tela.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,7 @@
 const express = require('express');
 const server = express();
 
+    server.get('/', (request, response) => {
+        return response.send('Hello World');        
+    });
     server.listen(3333);
