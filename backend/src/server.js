3e9c1b98ff2632f2d8e22cda69f361b423d12119
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-03-23 14:13:19 -0300

    Implementação do Mongoose e conexão com o Banco de Dados MongoDB.
    Importada a nova Dependência Mongoose no arquivo "src/server.js" antes das Rotas (após a importação do Express);
    Conectado o Back-End com o Banco de Dados "omnistack8" na MongoDB, através do método "mongoose.connect" já com a Autenticação do Administrador e o Nome da Collection configuradas na URL;
    Necessário, como segundo parâmetro, um Objeto com duas propriedades específicas: "useNewUrlParser" e "useUnifiedTopology" com os valores: "true", devido à depreciação de esquemas da URL na versão atual do Mongoose.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 const server = express();
 
-    server.use(express.json());
-    server.use(routes);
-    server.listen(3333);
+    mongoose.connect('mongodb+srv://omnistack_dev:KcAtSiNm0O@mavericks.pwlbv.mongodb.net/omnistack8?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+
+        server.use(express.json());
+        server.use(routes);
+        server.listen(3333);
