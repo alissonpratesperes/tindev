c37965554c53188d2e55659bde12141e4a5b4053
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-03-23 16:54:27 -0300

    "DevController.js" armazenando os Devs no Banco de Dados.
    Importada a Model "Dev" na "DevController.js", visto que a Controller irá trabalhar com a Requisição/Resposta e a Model (que é acionada automaticamente pelo Controller) trabalhará com o Schema de Armazenamento das informações dos Devs no Banco de Dados;
    Utilizando a Desestruturação do JavaScript, declaradas as informações configuradas na Model para serem coletadas pelo Axios, armazenadas na Constante apiResponse através do ".data" quando feita a Requisição à API do GitHub;
    Utilizando o recurso de Short Syntax do JavaScript, ainda dentro da Desestruturação do GET das informações, transformamos o atributo "avatar_url" que contém a Thumb do Dev em "avatar" que é assim que temos configurado na Model para armazenamento no Banco de Dados. Também fizemos essa configuração no Método "Dev.create" para o "user" que já temos através da própria requisição pelo "username";
    Declarada a Constante "dev" que aguardará (await) a criação do Dev no Banco de Dados (com o Método Dev.create) e receberá as informações referentes. Que serão retornadas através do "return" com um JSON no Insomnia.

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,9 +1,12 @@
 const axios = require('axios');
+const Dev = require('../models/Dev');
 
     module.exports = {
         async store(request, response) {
             const { username } = request.body;
             const apiResponse = await axios.get(`https://api.github.com/users/${username}`);
-                return response.json(apiResponse.data);
+            const { name, bio, avatar_url: avatar } = apiResponse.data;    
+            const dev = await Dev.create({ name, user: username, bio, avatar });    
+                return response.json(dev);
         }
     };
