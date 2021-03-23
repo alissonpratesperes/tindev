0b06b2a4a8c2cfcedaba9b75e37b8918944a79fe
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-03-23 10:02:16 -0300

    Estrutura de Dados JSON na Rota - Raiz "/" - da Aplicação.
    Como criaremos uma API utilizando a plataforma NodeJS, será necessário utilizarmos uma estrutura de comunicação unificada, que tanto o nosso Back-End, quanto o Front-End e Mobile consigam entender, nesse caso utilizaremos o JSON;
    O JSON é conhecido como JavaScript Object Notation, possui a identidade de um Objeto JavaScript, e conterá as respostas com os conteúdos das Requisições realizadas na Aplicação.

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const server = express();
 
     server.get('/', (request, response) => {
-        return response.send(`Hello ${request.query.name}`);        
+        return response.json({ message: `Hello ${request.query.name}` });        
     });
     server.listen(3333);
