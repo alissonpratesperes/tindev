e35e673455895d11796a7a6f5a3c4b8c5fa01e02
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-03-23 10:52:56 -0300

    Segunda Rota - Devs "/devs" - na Aplicação.
    Criada a segunda Rota "/devs" utilizando Método POST (cadastro de dados) com os parâmetros "request" (todas as informações referentes à Requisição do usuário) e "response" (retorno da Resposta à Requisição do usuário);
    Criado o retorno com o parâmetro "response" que retorna um JSON com o valor "ok: true" ao mesmo tempo executando o "console.log" no VS Code dos dados que foram enviados na "request.body" feita no Insomnia - Cliente API para o Debug durante o Desenvolvimento da Aplicação, porque o Browser não executa rotas "/post" por padrão, apenas rotas "/get".

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,5 +4,9 @@ const routes = express.Router();
     routes.get('/', (request, response) => {
         return response.json({ message: `Hello ${request.query.name}` });        
     });
+    routes.post('/devs', (request, response) => {
+        console.log(request.body);
+            return response.json({ ok: true });
+    });
 
         module.exports = routes;
