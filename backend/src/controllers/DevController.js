e739acd04215d7419cc28eace2ac0a7fd4a4c374
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-03-24 10:29:33 -0300

    Operação Idempotente no "DevController.js".
    Para não criar um registro duplicado, agora a Aplicação verifica se esse usuário já existe, e se existir ela apenas o retorna - e isso se chama Operação Idempotente, ou seja, independente da quantidade de Requests que forem feitas com os mesmos parâmetros, o registro não duplicará as operações dentro do Sistema.

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -4,6 +4,8 @@ const Dev = require('../models/Dev');
     module.exports = {
         async store(request, response) {
             const { username } = request.body;
+            const userExists = await Dev.findOne({ user: username });
+                if (userExists) { return response.json(userExists); }
             const apiResponse = await axios.get(`https://api.github.com/users/${username}`);
             const { name, bio, avatar_url: avatar } = apiResponse.data;    
             const dev = await Dev.create({ name, user: username, bio, avatar });    
