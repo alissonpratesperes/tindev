48b9ad72c698b60228d30b4be5ba04eacc7afd22
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-03-25 16:23:36 -0300

    Match entre os Desenvolvedores no "LikeController.js".
    Criada Estrutura Condicional para o Sistema de Match da Aplicação (inserida antes do Like ser armazenado no Array de Likes do "loggedDev"). Então, quando o "loggedDev" dar o Like no "targetDev" será verificado com o Método ".includes" se o Array de Likes do "targetDev" possui o "_id" (no padrão Mongoose - MongoDB) do "loggedDev". Em palavras mais simples, quando o "loggedDev" estiver dando um Like no "targetDev" ainda no "targetDev" será verificado se também contém o ID de Like do "loggedDev" - Se essa Condicional for atendida, será executado o "console.log" de Match entre os dois Desenvolvedores.

--- backend/src/controllers/LikeController.js
+++ backend/src/controllers/LikeController.js
@@ -7,6 +7,7 @@ const Dev = require('../models/Dev');
             const loggedDev = await Dev.findById(user);
             const targetDev = await Dev.findById(devId);
                 if(!targetDev) { return response.status(400).json({ error: 'This Developer does not exists.' }); }
+                if(targetDev.likes.includes(loggedDev._id)) { console.log('Match.') }  
                     loggedDev.likes.push(targetDev._id);
                         await loggedDev.save();
                 return response.json(loggedDev);
--- backend/src/models/Dev.js
+++ backend/src/models/Dev.js
@@ -5,7 +5,7 @@ const DevSchema = new Schema({
     bio: String,
     avatar: { type: String, required: true },
     likes: [{ type: Schema.Types.ObjectId, ref: 'Dev' }],
-    deslikes: [{ type: Schema.Types.ObjectId, ref: 'Dev' }]
+    dislikes: [{ type: Schema.Types.ObjectId, ref: 'Dev' }]
 }, { timestamps: true });
 
     module.exports = model('Dev', DevSchema);
