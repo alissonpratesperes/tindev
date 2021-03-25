33a39a45ecfbbb0ecb164f96dfd19ec545d2eba6
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-03-25 10:25:59 -0300

    Lógica inicial dos Likes entre os Desenvolvedores no "LikeController.js"..
    No arquivo "src/controllers/LikeController.js", implementado então o Método STORE com os parâmetros "request" (todas as informações referentes à Requisição do usuário) e "response" (retorno da Resposta à Requisição do usuário) que retorna um JSON com o conteúdo: "ok: true" para as validações iniciais de funcionamento;
    Utilizando o Método "console.log", na primeira linha da função de STORE, obtemos o ID do Dev (criado pelo Mongoose no cadastro) que recebeu o Like, através do "request.params" (Parâmetro da Requisição - Na Rota) sobre o "devId";
    Utiilizando o Método "console.log", na segunda linha da função de STORE, obtemos o ID do Dev (criado pelo Mongoose no cadastro) que deu o Like, através do "request.headers" (Cabeçalho da Requisição - No Insomnia) sobre o "user";
    Importado o "LikeController.js" no arquivo de Rotas "src/routes.js" e criada a Segunda Rota de POST para os Devs, dessa vez com o Parâmetro ":devId" sendo passado na Rota com o "/likes" no final. Essa rota irá acessar os Likes do Dev referente ao ID passado nela.

--- backend/src/controllers/LikeController.js
+++ backend/src/controllers/LikeController.js
@@ -1,7 +1,9 @@
 const Dev = require('../models/Dev');
 
     module.exports = {
-        store() {
-            
+        store(request, response) {
+            console.log(request.params.devId);
+            console.log(request.headers.user);
+                return response.json({ ok: true });
         }
     }
