88b2a7b9148ddde42db817605508d6dd457082c9
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-03-26 10:50:39 -0300

    Métodos "console.log" para Output de interações na Aplicação.
    Implementados os Métodos "console.log" para o Output dos Status das Requests/Responses no Console do VS Code: na Estrutura condicional da Operação Idempotente (que verifica usuário existente) e na Operação de Criação do Usuário (que cadastra o usuário). Assim é possível Aplicação dar o Output conforme interagimos com as Informações.

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -5,10 +5,11 @@ const Dev = require('../models/Dev');
         async store(request, response) {
             const { username } = request.body;
             const userExists = await Dev.findOne({ user: username });
-                if (userExists) { return response.json(userExists); }
+                if (userExists) { console.log(`User "${username}" already exists.`); return response.json(userExists); }
             const apiResponse = await axios.get(`https://api.github.com/users/${username}`);
             const { name, bio, avatar_url: avatar } = apiResponse.data;    
-            const dev = await Dev.create({ name, user: username, bio, avatar });    
-                    return response.json(dev);
+            const dev = await Dev.create({ name, user: username, bio, avatar });
+                    console.log(`User "${username}" created.`);    
+                        return response.json(dev);
         }
     };
