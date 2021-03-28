a435061202bc193cf60b1aad6bdb13194953e641
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-03-28 18:23:25 -0300

    Quarta nova Rota, de Listagem para todos os Desenvolvedores que o "loggedDev" não interagiu e finalização do desenvolvimento do Back-End.
    Criada a Quarta Rota da Aplicação, que utiliza o já importado "DevController.js" e utiliza o Método GET na Rota "/devs" com o Método Index no Controller para fazer a listagem de todos os Desenvolvedores em que o Dev Logado ainda não interagiu, seja com Like e/ou Dislike;
    No Controller "src/controllers/DevController.js", criado novo Método Assíncrono Index com os parâmetros "request" (todas as informações referentes à Requisição do usuário) e "response" (retorno da Resposta à Requisição do usuário) que coleta o "user" de "request.headers" (Cabeçalhos da Requisição - Usuário Logado) e armazena em uma Constante;
    Com a constante "loggedDev" dentro dessa função/método, se busca pelo ID com o Método "Dev.findById" a instância desse Usuário Logado na Aplicação no Banco de Dados e se tem todas as informações dele, por exemplo as d e Like e Dislike que foram tratadas nesse Desenvolvimento;
    Então buscaremos dentro do Banco de Dados, todos os usuários que não são o "loggedDev" (com o - Not Equal "$ne: user"), não são os que receberam "Like" (com o - Not In "$nin: loggedDev.likes") do "loggedDev" e/ou os que receberam "Dislike"(com o - Not In "$nin: loggedDev.dislikes") do "loggedDev", ou seja, trabalharemos com três filtros/condições que devem ser atendidas nessa Listagem através do Método "$and" do MongoDB, por volta desses três Objetos e que os armazenará na Constante "users";
    Basicamente, essa Query irá retornar todos os Desenvolvedores que não tenham recebido Like/Dislike do "loggedDev" e que também não o sejam;
    Configurado o return com o Parâmetro "response" com o Método JSON trazendo todos os Devs armazenados na Constante "users" que atendam às Condicionais implementadas.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,6 +4,7 @@ const LikeController = require('./controllers/LikeController');
 const DislikeController = require('./controllers/DislikeController');
 const routes = express.Router();
 
+    routes.get('/devs', DevController.index);
     routes.post('/devs', DevController.store);
     routes.post('/devs/:devId/likes', LikeController.store);
     routes.post('/devs/:devId/dislikes', DislikeController.store);
