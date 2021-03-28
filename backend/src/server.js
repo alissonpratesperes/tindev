3b3c5c1338914fe8b5517704150ab93a3ab22921
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-03-28 18:37:09 -0300

    Configuração do CORS no Back-End.
    No arquivo "src/server.js" importada a nova Dependência CORS instalada anteriormente, antes da importação das Rotas;
    Antes de qualquer Instrução do Servidor (sempre tem que vir antes das Configurações das Rotas), configurado o Método "server.use" para o Servidor utilizar o CORS para as Requisições, através da Função "cors()";
    Finalizadas essas Configurações o Back-End está 100% preparado para fornecer os dados que serão consumidos tanto pelo Front-End feito em React quanto pelo Mobile feito em React Native.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 const server = express();
 
     mongoose.connect('mongodb+srv://omnistack_dev:KcAtSiNm0O@mavericks.pwlbv.mongodb.net/omnistack8?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
+        server.use(cors());
         server.use(express.json());
         server.use(routes);
         server.listen(3333);
