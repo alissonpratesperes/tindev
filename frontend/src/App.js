d1940aad3d74691f8d36e44bc8bdd3070a0143fe
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-03-29 11:26:58 -0300

    Logotipo no Front-End.
    Criada nova pasta "src/assets" que por enquanto, contém já Logotipo "Tindev" da Aplicação, em SVG (Scalable Vector Graphics), para melhor qualidade em qualquer tamanho de tela;
    Importada então, a Logo no arquivo "src/App.js" e armazenada na Variável "logo" que é utilizada na Tag "<img/>" entre as Chaves "{}", sempre em React utiliza-se sempre as "{}" Chaves para inserir variáveis em Tags e/ou Textos que serão renderizados nas Páginas.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
+import React from 'react';
 import './App.css';
+import logo from './assets/logo.svg';
 
   function App() {
     return (
-      <h1> Hello Rocketseat </h1>
+      <img src={logo} alt="Tindev"/>
     );
   }
 
