76606fcc8c2f2e60cc708d87dffb0889077a4c62
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-03-29 13:41:35 -0300

    Renderização da Primeira Página - "Login.js" na Rota Raiz "/" da Aplicação.
    Como a Página de Login será a Primeira a ser renderizada em Tela de acordo com o fluxo de interação na Aplicação, sendo na Rota Raiz "/", criado anteriormente o arquivo "src/pages/Login.js" que será o Componente de Login e que irá renderizar essa tela;
    Essa Página agora é tratada como Componente pois foi importada dentro da Variável "Login" no arquivo "src/App.js" e acabou virando o Componente "<Login />". Como todo Componente é uma Função que retorna HTML, no arquivo "src/pages/Login.js" foi criada a Função "Login()" que retorna um HTML com mais de uma linha e que será desenvolvido dentro do "return()", que nesse caso apenas contém a Logo, importada para a Variável "logo" e que por enquanto está somente nessa tela.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-import logo from './assets/logo.svg';
+import Login from './pages/Login';
 
   function App() {
     return (
-      <img src={logo} alt="Tindev"/>
+      <Login />
     );
   }
 
---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from '../assets/logo.svg';
+
+    function Login() {
+        return (
+            <img src={logo} alt="Tindev"/>
+        );
+    }
+
+        export default Login;
