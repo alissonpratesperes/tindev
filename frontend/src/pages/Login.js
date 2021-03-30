1574430ed92de20afe4a909cdb8ccb1bc08af3c0
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-03-29 21:49:43 -0300

    React Hook - useState - na Tela de Login da Aplicação.
    No ReactJS temos o Conceito de Estado, que é toda e qualquer informação que o Componente vai manipular, que ele precisa modificar e/ou acessar de alguma forma. Então, utilizando a Desestruturação do JavaScript foi importado de dentro do Módulo React o "useState";
    Para trabalharmos então com o Estado, primeiro deve-se iniciar a Variável, nesse caso "username" vazia conforme "useState('')" que sempre retorna dois valores em formato de Vetor: o Estado Atual - "username" e a Função de Mudança - "setUsername" que serão armazenados em uma Constante;
    Então, o React Hook "useState" inicialmente implementado no Input, para coletarmos qual o Usuário está logando na Aplicação, recebe duas configurações principais: o "value={}" que recebe o "username" (estado atual) e "onChange={}" que recebe o evento que executa a Função "setUsername" (muda o estado atual) com o "event.target.value" (valor digitado dentro do Input) enviado como parâmetro. Dessa forma o ReactJS atualizará o Estado da Variável "username" automaticamente com o valor que for digitado no Input dessa tela de Login;
    Criada a Função "handleSubmit" que será disparada quando for feito o Submit desse Form, conforme declarado na Tag <form/> o nome dessa Função no Atributo "onSubmit={}". Essa Função, por enquanto com o "event.preventDefault()", previne o comportamento padrão do Formulário ao Submitar (bloqueia o redirecionamento/atualização da tela), e executa o Método "console.log" para exibição do Estado da Variável "username" na Console do Browser.

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 import logo from '../assets/logo.svg';
 
     function Login() {
-        return (
-            <div className="login-container">
-                <form>
-                    <img src={logo} alt="Tindev"/>
-                        <input type="text" placeholder="Insira seu usuário do GitHub"/>
-                        <button type="submit"> Entrar </button>
-                </form>
-            </div>
-        );
+        const [username, setUsername] = useState('');
+
+            function handleSubmit(event) {
+                event.preventDefault();
+                    console.log(username);
+            }
+
+                return (
+                    <div className="login-container">
+                        <form onSubmit={handleSubmit}>
+                            <img src={logo} alt="Tindev"/>
+                                <input type="text" placeholder="Insira seu usuário do GitHub" value={username} onChange={event => setUsername(event.target.value)}/>
+                                <button type="submit"> Entrar </button>
+                        </form>
+                    </div>
+                );
     }
 
         export default Login;
