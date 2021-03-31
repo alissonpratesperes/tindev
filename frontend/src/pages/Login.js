b01da6436df25484a7ab3d3087c90a01d4b7ad15
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-03-31 15:45:08 -0300

    Props - Propriedade "history" na Função "Login()" e Navegação com Método ".push".
    Quando é utilizado a Dependência React Router Dom e quando declaramos as Rotas no arquivo "src/routes.js", que no caso dessa Aplicação são as "Login" e "Main", automaticamente essas Rotas herdam uma propriedade chamada "history" de dentro/através do React Router Dom. A propriedade "history" serve para realizarmos a "navegação" dos Usuários, e junto do Método ".push" é feito o Roteamento para a Rota entre "('')", nesse caso da tela de Login para a "/main";
    Utilizando a Desestruturação do JavaScript, passamos a Propriedade "history" para a Função "Login" onde executa o Método "console.log" para informar o "Usuário Logado", faz o  ".push" para a Rota "/main".

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -2,18 +2,19 @@ import React, { useState } from 'react';
 import './Login.css';
 import logo from '../assets/logo.svg';
 
-    function Login() {
+    function Login({ history }) {
         const [username, setUsername] = useState('');
 
             function handleSubmit(event) {
                 event.preventDefault();
-                    console.log(username);
+                    console.log(`User "${username}" logged.`);
+                        history.push('/main');
             }
 
                 return (
                     <div className="login-container">
                         <form onSubmit={handleSubmit}>
-                            <img src={logo} alt="Tindev"/>
+                            <img src={logo} alt="tindev"/>
                                 <input type="text" placeholder="Insira seu usuário do GitHub" value={username} onChange={event => setUsername(event.target.value)}/>
                                 <button type="submit"> Entrar </button>
                         </form>
