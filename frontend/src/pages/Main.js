ba58c5e11599b6d3a8a286ac7c2bfe7975e2d70b
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-04-01 16:50:44 -0300

    Implementação inicial dos Likes e Dislikes nos "targetDevs" pelo "loggedDev" na Página "Main.js".
    Antes do "return();" da Página "src/pages/Main.js", criadas as duas Funções assíncronas: "handleLike()" e "handleDislike()", nessa Aplicação padronizamos as Funções que dependem da interação do usuário com o prefixo "handle". E nessas duas Funções, como parâmetro passado o ID do Desenvolvedor que o Desenvolvedor logado dará o Like/Dislike, uma vez que o ID do Desenvolvedor logado já se tem através do "match.params.id" que está incluso na Rota "/dev";
    Em ambas as Funções executado o Método "console.log" utilizando as Template Strings dos eventos de "Like" e "Dislike" com o ID dos "targetDevs" (Desenvolvedores que receberam o Like/Dislike), assim padronizando a "minha versão" dessa Aplicação com os Logs de quase todas as interações;
    Chamadas essas Funções através da Propriedade "onClick" nos "Buttons" respectivos delas, que recebem uma Arrow Function (para executar somente quando houver o evento de Click - se fosse descrita como Função padrão, executaria assim que o Componente "Main.js" fosse renderizado em tela, automaticamente), então como Parâmetros dessas Funções passados os IDs dos "targetDevs" através do Método "user._id" - Assim quando o "loggedDev" dar um Like/Dislike em algum dos "targetDevs", será exibido na Console do Browser em qual ID inputou esses eventos.

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -8,35 +8,42 @@ import api from '../services/api';
     function Main({ match }) {
         const [users, setUsers] = useState([]);
 
-        useEffect(() => {
-            async function loadUsers() {
-                const response = await api.get('/devs', { headers: { user: match.params.id } });
-                    console.log(response.data);
-                        setUsers(response.data);
-            }
-            loadUsers();
-        }, [match.params.id]);
+            useEffect(() => {
+                async function loadUsers() {
+                    const response = await api.get('/devs', { headers: { user: match.params.id } });
+                        console.log(response.data);
+                            setUsers(response.data);
+                }
+                loadUsers();
+            }, [match.params.id]);
 
-            return (
-                <div className="main-container">
-                    <img src={logo} alt="tindev"/>
-                        <ul>
-                            {users.map(user => (
-                                <li key={user._id}>
-                                    <img src={user.avatar} alt={user.name}/>
-                                        <footer>
-                                            <strong> {user.name} </strong>
-                                                <p> {user.bio} </p>
-                                        </footer>
-                                            <div className="buttons">
-                                                <button type="button" className="dislike"> <img src={dislike} alt="Dislike"/> </button>
-                                                <button type="button" className="like"> <img src={like} alt="Like"/> </button>
-                                            </div>
-                                </li>
-                            ))} 
-                        </ul>
-                </div>
-            );
+                async function handleDislike(id) {
+                    console.log(`User "${id}" disliked.`);
+                }
+                async function handleLike(id) {
+                    console.log(`User "${id}" liked.`)
+                }
+
+                    return (
+                        <div className="main-container">
+                            <img src={logo} alt="tindev"/>
+                                <ul>
+                                    {users.map(user => (
+                                        <li key={user._id}>
+                                            <img src={user.avatar} alt={user.name}/>
+                                                <footer>
+                                                    <strong> {user.name} </strong>
+                                                        <p> {user.bio} </p>
+                                                </footer>
+                                                    <div className="buttons">
+                                                        <button type="button" className="dislike" onClick={() => handleDislike(user._id)}> <img src={dislike} alt="Dislike"/> </button>
+                                                        <button type="button" className="like" onClick={() => handleLike(user._id)}> <img src={like} alt="Like"/> </button>
+                                                    </div>
+                                        </li> 
+                                    ))} 
+                                </ul>
+                        </div>
+                    );
     }
 
         export default Main;
