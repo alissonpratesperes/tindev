542fe9de4c1617f13f0f7ab34804bf6ffec1b7c2
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-04-01 10:08:49 -0300

    Segunda requisição - do tipo GET na Rota "/devs" na Página "Main.js" para o Back-End.
    Utilizando o Método dos React Hooks "useEffect", será realizada uma Chamada à API assim que o Componente for renderizado em tela, nesse caso o Componente sendo a Página "Main.js". O "useEffect" é uma Função que sempre será escrita e executada antes do Método "return();", e recebe sempre dois Parâmetros: O primeiro Parâmetro é a Função que será executada (Arrow Function), o segundo Parâmetro é Quando que será executada - Array de Dependências - que geralmente pode receber as Variáveis que serão monitoradas pelo "useEffect". Assim, toda a vez que essas Variáveis forem alteradas, a Função (primeiro Parâmetro) é executada novamente (lembrando que caso esse segundo Parâmetro - Array - estiver vazio, ela será executada somente uma vez dentro do Componente);
    Nesse caso, nas Dependências do "useEffect" será passada a Variável "match.params.id", toda vez que o ID da URL for alterado essa Função será executada novamente;
    Criada a Função assíncrona "loadUsers" que através do Método GET realiza a requisição à "api" na Rota "/devs" com a Variável "user" (que contém o ID do Dev logado - match.params.id) nos Headers da Requisição e armazena os dados dos Desenvolvedores na Constante "response";
    Utilizado o Método "console.log" no atributo ".data" da Constante "response" para exibição dos dados na Console do Browser dos resultados (dos Desenvolvedores) dentro de um Array, obtidos após a realização da requisição à API.

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,61 +1,70 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Main.css';
 import logo from '../assets/logo.svg';
 import dislike from '../assets/dislike.svg';
 import like from '../assets/like.svg';
+import api from '../services/api';
 
     function Main({ match }) {
-        return (
-            <div className="main-container">
-                <img src={logo} alt="tindev"/>
-                    <ul>
-                        <li>
-                            <img src="https://avatars.githubusercontent.com/u/52282116?v=4" alt="Developer Avatar"/>
-                                <footer>
-                                    <strong> Alisson Prates Peres </strong>
-                                        <p> Newbie but focused. </p>
-                                </footer>
-                                    <div className="buttons">
-                                        <button type="button" className="dislike"> <img src={dislike} alt="Dislike"/> </button>
-                                        <button type="button" className="like"> <img src={like} alt="Like"/> </button>
-                                    </div>
-                        </li>
-                        <li>
-                            <img src="https://avatars.githubusercontent.com/u/2254731?v=4" alt="Developer Avatar"/>
-                                <footer>
-                                    <strong> Diego Fernandes </strong>
-                                        <p> CTO at @Rocketseat. Passionate about education and changing people's lives through programming. </p>
-                                </footer>
-                                    <div className="buttons">
-                                        <button type="button" className="dislike"> <img src={dislike} alt="Dislike"/> </button>
-                                        <button type="button" className="like"> <img src={like} alt="Like"/> </button>
-                                    </div>
-                        </li>
-                        <li>
-                            <img src="https://avatars.githubusercontent.com/u/6643122?v=4" alt="Developer Avatar"/>
-                                <footer>
-                                    <strong> Mayk Brito </strong>
-                                        <p> An instructor focused on helping people start programming for web - #html #css #javascript #sql #react #nodejs #fullstack </p>
-                                </footer>
-                                    <div className="buttons">
-                                        <button type="button" className="dislike"> <img src={dislike} alt="Dislike"/> </button>
-                                        <button type="button" className="like"> <img src={like} alt="Like"/> </button>
-                                    </div>
-                        </li>
-                        <li>
-                            <img src="https://avatars.githubusercontent.com/u/4248081?v=4" alt="Developer Avatar"/>
-                                <footer>
-                                    <strong> Filipe Deschamps </strong>
-                                        <p> Vou fazer você se apaixonar por programação! </p>
-                                </footer>
-                                    <div className="buttons">
-                                        <button type="button" className="dislike"> <img src={dislike} alt="Dislike"/> </button>
-                                        <button type="button" className="like"> <img src={like} alt="Like"/> </button>
-                                    </div>
-                        </li>
-                    </ul>
-            </div>
-        );
+        useEffect(() => {
+            async function loadUsers() {
+                const response = await api.get('/devs', { headers: { user: match.params.id } });
+                    console.log(response.data);
+            }
+            loadUsers();
+        }, [match.params.id]);
+
+            return (
+                <div className="main-container">
+                    <img src={logo} alt="tindev"/>
+                        <ul>
+                            <li>
+                                <img src="https://avatars.githubusercontent.com/u/52282116?v=4" alt="Developer Avatar"/>
+                                    <footer>
+                                        <strong> Alisson Prates Peres </strong>
+                                            <p> Newbie but focused. </p>
+                                    </footer>
+                                        <div className="buttons">
+                                            <button type="button" className="dislike"> <img src={dislike} alt="Dislike"/> </button>
+                                            <button type="button" className="like"> <img src={like} alt="Like"/> </button>
+                                        </div>
+                            </li>
+                            <li>
+                                <img src="https://avatars.githubusercontent.com/u/2254731?v=4" alt="Developer Avatar"/>
+                                    <footer>
+                                        <strong> Diego Fernandes </strong>
+                                            <p> CTO at @Rocketseat. Passionate about education and changing people's lives through programming. </p>
+                                    </footer>
+                                        <div className="buttons">
+                                            <button type="button" className="dislike"> <img src={dislike} alt="Dislike"/> </button>
+                                            <button type="button" className="like"> <img src={like} alt="Like"/> </button>
+                                        </div>
+                            </li>
+                            <li>
+                                <img src="https://avatars.githubusercontent.com/u/6643122?v=4" alt="Developer Avatar"/>
+                                    <footer>
+                                        <strong> Mayk Brito </strong>
+                                            <p> An instructor focused on helping people start programming for web - #html #css #javascript #sql #react #nodejs #fullstack </p>
+                                    </footer>
+                                        <div className="buttons">
+                                            <button type="button" className="dislike"> <img src={dislike} alt="Dislike"/> </button>
+                                            <button type="button" className="like"> <img src={like} alt="Like"/> </button>
+                                        </div>
+                            </li>
+                            <li>
+                                <img src="https://avatars.githubusercontent.com/u/4248081?v=4" alt="Developer Avatar"/>
+                                    <footer>
+                                        <strong> Filipe Deschamps </strong>
+                                            <p> Vou fazer você se apaixonar por programação! </p>
+                                    </footer>
+                                        <div className="buttons">
+                                            <button type="button" className="dislike"> <img src={dislike} alt="Dislike"/> </button>
+                                            <button type="button" className="like"> <img src={like} alt="Like"/> </button>
+                                        </div>
+                            </li>
+                        </ul>
+                </div>
+            );
     }
 
         export default Main;
