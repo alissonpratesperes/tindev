f6947a3732871032aaad4f454716e937348cedb4
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-04-02 23:44:58 -0300

    Primeiros passos com a Stack Mobile da Aplicação.
    Primeiros passos com a Stack Mobile da Aplicação, remoção de alguns Códigos-Fonte desnecessários e criação da Primeira tela renderizada no iPhone com o Expo.

--- mobile/App.js
+++ mobile/App.js
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  export default function App() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}> Hello OmniStack 8! </Text>
+      </View>
+    );
+  }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#7159C1'
+      },
+      text: {
+        fontWeight: 'bold',
+        color: '#FFFFFF',
+        fontSize: 20
+      }
+    });
+
