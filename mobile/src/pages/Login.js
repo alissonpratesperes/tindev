f7f54fc543027cf878199456eac151734580ff46
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-04-03 17:09:50 -0300

    Estrutura dos elementos e Estilizações CSS Iniciais na Página "Login.js" na Aplicação Mobile.
    Criada a pasta "src/assets" para os Assets da Aplicação Mobile, com o Logo em 3 tamanhos (com os 1x, @2x, @3x) sendo em ".png" porque por padrão o React Native não permite o uso de Imagens em SVG (Scalable Vector Graphics), e esses três tamanhos são gerenciados automaticamente pelo React Native por Device de acordo com a Densidade de Pixel (PPI) quando o Expo realizar o Bundle da Aplicação Mobile;
    Então no arquivo "src/pages/Login.js" importada a "logo" no arquivo "1x" que conforme o Device executar o Expo no Bundle ele irá gerenciar automaticamente pelo React Native qual das 3 "logo" deverão ser renderizadas;
    Importado o Componente "StyleSheet" e criada a Constante "styles" com o Objeto de Estilos através do Método ".create({})" inicialmente com a Propriedade "container" contendo os Estilos Iniciais da Tela de Login;
    Importados os Componentes "TextInput" (no React Native não existe Formulários) e "TouchableOpacity" para criar a Estrutura inicial da parte de Login na Aplicação assim como no Front-End Web. Criadas e desenvolvidas também as Propriedades CSS na Constante "styles" para esses elementos, contendo as Estilizações CSS Iniciais conforme passado em aula.

--- mobile/src/pages/Login.js
+++ mobile/src/pages/Login.js
@@ -1,6 +1,49 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
+import logo from '../assets/logo.png';
 
     export default function Login() {
-        return <View />
+        return (
+            <View style={styles.container}>
+                <Image source={logo}/>
+                    <TextInput placeholder="Insira seu usuário do GitHub" placeholderTextColor="#999999" style={styles.input}/>
+                        <TouchableOpacity style={styles.button}>
+                            <Text style={styles.buttonText}> Enviar </Text>
+                        </TouchableOpacity>
+            </View>
+        );
     }
+
+        const styles = StyleSheet.create({
+            container: {
+                flex: 1,
+                backgroundColor: '#F5F5F5',
+                justifyContent: 'center',
+                alignItems: 'center',
+                padding: 30,
+            },
+            input: {
+                height: 50,
+                alignSelf: 'stretch',
+                backgroundColor: '#FFFFFF',
+                borderWidth: 1,
+                borderColor: '#DDDDDD',
+                borderRadius: 4,
+                marginTop: 20,
+                paddingHorizontal: 15
+            },
+            button: {
+                height: 46,
+                alignSelf: 'stretch',
+                backgroundColor: "#Df4723",
+                borderRadius: 4,
+                marginTop: 10,
+                justifyContent: 'center',
+                alignItems: 'center'
+            },
+            buttonText: {
+                color: '#FFFFFF',
+                fontWeight: 'bold',
+                fontSize: 16
+            }
+        });
