eb9a90fef995a122300e9cc35b4619b4a121632f
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-04-03 16:37:34 -0300

    Início do desenvolvimento da Navegação e Páginas da Aplicação Mobile.
    Na Página "src/pages/Login.js" apenas criada a Função "Login()" que retorna uma <View /> vazia para os testes iniciais. Aqui no desenvolvimento da Stack Mobile o "export default" está integrado (e provavelmente sempre ficará nas próximas) na declaração da Função do Componente;
    No arquivo principal do Aplicativo "src/index.js" limpado todo o Código-Fonte desenvolvido nos testes anteriores, importado o Componente/Página de Login "src/pages/Login.js" e no "return()" declarado esse Componente, por enquanto, assim que o Aplicativo abrir a Página de Login será carregada diretamente como primeiro Componente renderizado em tela.

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,24 +1,8 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import Login from './pages/Login';
 
   export default function App() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.text}> Hello OmniStack 8! </Text>
-      </View>
+      <Login />
     );
   }
-
-    const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#FFFFFF'
-      },
-      text: {
-        fontWeight: 'bold',
-        color: '#7159C1',
-        fontSize: 20
-      }
-    });
