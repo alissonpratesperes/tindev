4ba93ff85112c1709934e96c1d85a2a287625cb8
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-04-05 10:52:39 -0300

    AsyncStorage na Página "Login", validação do "loggedDev" e finalização da Página de "Login" na Aplicação Mobile.
    No arquivo "src/services/api.js", alterado o IP para conexão do Device Físico/Stack Mobile com a API, uma vez que estamos em outro Ambiente de Desenvolvimento, onde o IP da Máquina HOST é diferente;
    Instalada nova Dependência AsyncStorage(@react-native-community/async-storage) para o armazenamento local de dados - no localStorage da Aplicação Mobile;
    Então, no arquivo "src/pages/Login.js", utilizada a Desestruturação do JavaScript para importar o React Hook "useEffect". Implementado na Função "handleLogin" o Método assíncrono - await - ".setItem" do AsyncStorage - assim que o Usuário/Desenvolvedor logar na Aplicação, armazenamos o "_id" do "loggedDev" na Variável "user" no localStorage do App Mobile;
    Caso o Usuário/Desenvolvedor fechar a Aplicação e inicializar novamente - ou Atualizar pelo Expo, utilizamos/executamos o React Hook "useEffect" - executando somente uma vez, conforme Array Vazio " [ ] " como segundo parâmetro - que acessa o AsyncStorage, busca com o Método ".getItem" se a Variável "user" contém o "_id" do "loggedDev" e caso houver essa informação, navega ele diretamente utilizando o Método ".navigate" para a Rota/Página "Main" com a Variável "user" sendo passada como segundo parâmetro - assim a página "Main" saberá quais dados carregar de acordo com o "_id" do "loggedDev".

--- mobile/src/pages/Login.js
+++ mobile/src/pages/Login.js
@@ -1,28 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
 import { KeyboardAvoidingView, Platform, Text, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
 import logo from '../assets/logo.png';
 import api from '../services/api';
 
     export default function Login({ navigation }) {
         const [user, setUser] = useState('');
-        
-            async function handleLogin() {
-                const response = await api.post('/devs', { username: user });
-                const { _id } = response.data;
-                    navigation.navigate('Main', { _id });
-                        console.log(`User "${user}" logged.`);
-                        console.log(`User "${user}" - ID "${_id}" navigated to Main Page."`);
-            }
 
-                return (
-                    <KeyboardAvoidingView behavior="padding" enabled={Platform.OS === 'ios'} style={styles.container}>
-                        <Image source={logo}/>
-                            <TextInput autoCapitalize="none" autoCorrect={false} placeholder="Insira seu usuário do GitHub" placeholderTextColor="#999999" style={styles.input} value={user} onChangeText={setUser}/>
-                                <TouchableOpacity onPress={handleLogin} style={styles.button}>
-                                    <Text style={styles.buttonText}> Entrar </Text>
-                                </TouchableOpacity>
-                    </KeyboardAvoidingView>
-                );
+            useEffect(() => {
+                AsyncStorage.getItem('user').then(user => { if(user) { navigation.navigate('Main', {user}) } });
+            }, []);
+        
+                async function handleLogin() {
+                    const response = await api.post('/devs', { username: user });
+                    const { _id } = response.data;
+                        await AsyncStorage.setItem('user', _id);
+                            navigation.navigate('Main', { _id });
+                                console.log(`User "${user}" logged.`);
+                                console.log(`User "${user}" - ID "${_id}" navigated to Main Page."`);
+                }
+
+                    return (
+                        <KeyboardAvoidingView behavior="padding" enabled={Platform.OS === 'ios'} style={styles.container}>
+                            <Image source={logo}/>
+                                <TextInput autoCapitalize="none" autoCorrect={false} placeholder="Insira seu usuário do GitHub" placeholderTextColor="#999999" style={styles.input} value={user} onChangeText={setUser}/>
+                                    <TouchableOpacity onPress={handleLogin} style={styles.button}>
+                                        <Text style={styles.buttonText}> Entrar </Text>
+                                    </TouchableOpacity>
+                        </KeyboardAvoidingView>
+                    );
     }
 
         const styles = StyleSheet.create({
