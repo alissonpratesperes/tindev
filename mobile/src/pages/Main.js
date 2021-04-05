5673e964b25252e6cf0bc39ef22ed7204bd3e92b
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-04-05 11:45:54 -0300

    Estrutura dos elementos e Estilizações CSS Iniciais na Página "Main.js" na Aplicação Mobile.
    Alocados na pasta "src/assets" os ícones de "Dislike" e "Like" em 3 tamanhos (com os 1x, @2x, @3x) sendo em ".png" porque por padrão o React Native não permite o uso de Imagens em SVG (Scalable Vector Graphics), e esses três tamanhos são gerenciados automaticamente pelo React Native por Device de acordo com a Densidade de Pixel (PPI) quando o Expo realizar o Bundle da Aplicação Mobile;
    Então no arquivo "src/pages/Main.js" importados os  ícones "dislike" e "like" no arquivo "1x" que conforme o Device executar o Expo no Bundle ele irá gerenciar automaticamente pelo React Native qual das 3 "logo" deverão ser renderizadas;
    Importado o Componente "StyleSheet" e criada a Constante "styles" com o Objeto de Estilos através do Método ".create({})" com todos os Objetos de Estilos iniciais padrão passados conforme a aula;
    Criada a estrutura inicial dos Elementos dessa Página "Main.js" para o padrão em que ela será, bem como o padrão dos botões de Dislike e Like, seguidos das Estilizações CSS Iniciais conforme passado em aula.

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,6 +1,112 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, SafeAreaView, Image, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import logo from '../assets/logo.png';
+import dislike from '../assets/dislike.png';
+import like from '../assets/like.png';
 
     export default function Main() {
-        return <View />
+        return (
+            <SafeAreaView style={styles.container}>
+                <Image style={styles.logo} source={logo}/>
+                    <View style={styles.cardsContainer}>
+                        <View style={[styles.card, { zIndex: 3 }]}>
+                            <Image style={styles.avatar} source={{ uri: 'https://avatars.githubusercontent.com/u/52282116?v=4' }}/>
+                                <View style={styles.footer}>
+                                    <Text style={styles.name}> Alisson Prates Peres </Text>
+                                    <Text style={styles.bio} numberOfLines={3}> Newbie but focused. Newbie but focused. Newbie but focused. Newbie but focused. Newbie but focused. Newbie but focused. Newbie but focused. Newbie but focused. </Text>
+                                </View>
+                        </View>
+                        <View style={[styles.card, { zIndex: 2 }]}>
+                            <Image style={styles.avatar} source={{ uri: 'https://avatars.githubusercontent.com/u/52282116?v=4' }}/>
+                                <View style={styles.footer}>
+                                    <Text style={styles.name}> Alisson Prates Peres </Text>
+                                    <Text style={styles.bio} numberOfLines={3}> Newbie but focused. Newbie but focused. Newbie but focused. Newbie but focused. Newbie but focused. Newbie but focused. Newbie but focused. Newbie but focused. </Text>
+                                </View>
+                        </View>
+                        <View style={[styles.card, { zIndex: 1 }]}>
+                            <Image style={styles.avatar} source={{ uri: 'https://avatars.githubusercontent.com/u/52282116?v=4' }}/>
+                                <View style={styles.footer}>
+                                    <Text style={styles.name}> Alisson Prates Peres </Text>
+                                    <Text style={styles.bio} numberOfLines={3}> Newbie but focused. Newbie but focused. Newbie but focused. Newbie but focused. Newbie but focused. Newbie but focused. Newbie but focused. Newbie but focused. </Text>
+                                </View>
+                        </View>
+                    </View>
+                    <View style={styles.buttonsContainer}>
+                        <TouchableOpacity style={styles.button}>
+                            <Image source={dislike}/>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.button}>
+                            <Image source={like}/>
+                        </TouchableOpacity>
+                    </View>
+            </SafeAreaView>
+        );
     }
+
+        const styles = StyleSheet.create({
+            container: {
+                flex: 1,
+                backgroundColor: '#F5F5F5',
+                alignItems: 'center',
+                justifyContent: 'space-between'
+            },
+            logo: {
+                marginTop: 30
+            },
+            cardsContainer: {
+                flex: 1,
+                alignSelf: 'stretch',
+                justifyContent: 'center',
+                maxHeight: 500,
+            },
+            card: {
+                borderWidth: 1,
+                borderColor: '#DDDDDD',
+                borderRadius: 8,
+                margin: 30,
+                overflow: 'hidden',
+                position: 'absolute',
+                left: 0,
+                top: 0,
+                right: 0,
+                bottom: 0
+            },
+            avatar: {
+                flex: 1,
+                height: 300
+            },
+            footer: {
+                backgroundColor: '#FFFFFF',
+                paddingHorizontal: 20,
+                paddingVertical: 15
+            },
+            name: {
+                fontSize: 16,
+                fontWeight: 'bold',
+                color: '#333333'
+            },
+            bio: {
+                fontSize: 14,
+                color: '#999999',
+                marginTop: 5,
+                lineHeight: 18
+            },
+            buttonsContainer: {
+                flexDirection: 'row',
+                marginBottom: 30
+            },
+            button: {
+                width: 50,
+                height: 50,
+                borderRadius: 25,
+                backgroundColor: '#FFFFFF',
+                justifyContent: 'center',
+                alignItems: 'center',
+                marginHorizontal: 20,
+                elevation: 2,
+                shadowColor: '#000000',
+                shadowOpacity: 0.05,
+                shadowRadius: 2,
+                shadowOffset: { width: 0, height: 2 }
+            },
+        });
