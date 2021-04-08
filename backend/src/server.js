0e7fb332123dc661421d2461749a92c111beb767
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-04-07 21:21:50 -0300

    Preparação inicial do Servidor Back-End para a Requisição HTTP e WebSocket.
    Trocado o nome da Constante "server", que recebe a Função do "express()" para "app" e atualizada essa nova Constante em todos os Middlewares no final do arquivo;
    Criada a nova Constante "server" que recebe o Módulo HTTP padrão do NodeJS "('http') - extraindo o Servidor HTTP de dentro do Microframework Express declarado na Constante "app" acima - para unirmos com um Servidor WebSocket, nesse caso ".Server" que recebe a Constante "(app)" como Parâmetro - aceitando tanto conexões WebSocket quanto conexões HTTP;
    No arquivo "src/server.js" importada a Biblioteca "socket.io" para dentro da Constante "io" que recebe também a Função "()" que receberá o Server HTTP, como Parâmetro "(server)";
    Agora a Aplicação "estaria pronta" para receber tanto Requisições HTTP quanto Requisições WebSocket.

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,11 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const routes = require('./routes');
-const server = express();
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
 
     mongoose.connect('mongodb+srv://omnistack_dev:KcAtSiNm0O@mavericks.pwlbv.mongodb.net/omnistack8?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
-        server.use(cors());
-        server.use(express.json());
-        server.use(routes);
-        server.listen(3333);
+        app.use(cors());
+        app.use(express.json());
+        app.use(routes);
+
+            server.listen(3333);
