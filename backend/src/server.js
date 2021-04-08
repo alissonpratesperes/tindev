7ee7fcd659bf123398cda64a31305a4f2a21fcae
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-04-08 08:41:25 -0300

    Instalada nova Dependência - Socket.IO-Client - no Front-End da Aplicação.
    No arquivo "src/server.js" do Back-End, iniciada a implementação do Protocolo WebSocket;
    Através do Método ".on" na Constante "io", passado como primeiro parâmetro a "connection" - ou seja, toda vez que alguém se conectar à Aplicação pelo Protocolo WebSocket - para o Back-End receber o próprio Socket "socket => {}", onde nesse caso executará o Método "console.log" para 'Nova conexão' que recebe como segundo parâmetro o "id" do Socket que se conectou;
    Alterado no diretório "mobile", no arquivo "mobile/src/services/api.js", o Endereço de IP da "baseURL" de conexão com o Axios devido à mudança de Ambiente de Desenvolvimento;
    Instalada nova Dependência Socket.IO-Client no diretório do Front-End para o início da comunicação via Protocolo WebSocket entre o Front-End e o Back-End, uma vez que o Mobile será a última Stack a receber essa implementação.

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,11 +6,14 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
+    io.on('connection', socket => {
+        console.log('Nova conexão', socket.id);
+    });
 
-    mongoose.connect('mongodb+srv://omnistack_dev:KcAtSiNm0O@mavericks.pwlbv.mongodb.net/omnistack8?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+        mongoose.connect('mongodb+srv://omnistack_dev:KcAtSiNm0O@mavericks.pwlbv.mongodb.net/omnistack8?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
-        app.use(cors());
-        app.use(express.json());
-        app.use(routes);
+            app.use(cors());
+            app.use(express.json());
+            app.use(routes);
 
-            server.listen(3333);
+                server.listen(3333);
